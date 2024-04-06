f3c2a957ebdb017f9da951f083a7fc6ccb5de441
admin <admin@admin.com>
2024-04-06 15:42:57 +0200

    routers

---
+++ handlers/population_by_city.py
@@ -0,0 +1,11 @@
+from aiogram import Router, F
+from aiogram.filters import Command, StateFilter
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.types import Message, ReplyKeyboardRemove
+
+from keyboards.common_keyboards import main_keyboard
+from states import UserStates
+
+
+router = Router()
