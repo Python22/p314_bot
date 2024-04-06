1f9660b504b47c6c792003f2b7830d8883e63f1e
admin <admin@admin.com>
2024-04-06 16:15:15 +0200

    weather 0.1

--- handlers/weather_by_city.py
+++ handlers/weather_by_city.py
@@ -3,9 +3,22 @@ from aiogram.filters import Command, StateFilter
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.types import Message, ReplyKeyboardRemove
+import requests
+import httpx
+import aiohttp
+from datetime import datetime
 
 from keyboards.common_keyboards import main_keyboard
 from states import UserStates
 
 
 router = Router()
+
+
+@router.message(UserStates.user_choice_button, F.text.lower() == "погода")
+async def need_weather(message: Message, state: FSMContext):
+    await message.answer(
+        text="Введи название города, чтобы узнать в нём погоду:",
+        reply_markup=ReplyKeyboardRemove()
+    )
+    await state.set_state(UserStates.user_choice_city)
