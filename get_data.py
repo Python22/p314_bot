28d911950817ade15af49e1a57a0ef0056245aa3
admin <admin@admin.com>
2024-04-06 19:02:48 +0200

    get_data_countries

--- get_data.py
+++ get_data.py
@@ -3,34 +3,30 @@ import requests
 import json
 
 
-url = "https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D0%B3%D0%BE%D1%81%D1%83%D0%B4%D0%B0%D1%80%D1%81%D1%82%D0%B2_%D0%B8_%D0%B7%D0%B0%D0%B2%D0%B8%D1%81%D0%B8%D0%BC%D1%8B%D1%85_%D1%82%D0%B5%D1%80%D1%80%D0%B8%D1%82%D0%BE%D1%80%D0%B8%D0%B9_%D0%BF%D0%BE_%D0%BD%D0%B0%D1%81%D0%B5%D0%BB%D0%B5%D0%BD%D0%B8%D1%8E"
+def get_data_from_countries():
+    url = "https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D0%B3%D0%BE%D1%81%D1%83%D0%B4%D0%B0%D1%80%D1%81%D1%82%D0%B2_%D0%B8_%D0%B7%D0%B0%D0%B2%D0%B8%D1%81%D0%B8%D0%BC%D1%8B%D1%85_%D1%82%D0%B5%D1%80%D1%80%D0%B8%D1%82%D0%BE%D1%80%D0%B8%D0%B9_%D0%BF%D0%BE_%D0%BD%D0%B0%D1%81%D0%B5%D0%BB%D0%B5%D0%BD%D0%B8%D1%8E"
 
-page = requests.get(url).text
-soup = BeautifulSoup(page, 'html.parser')
-countries_table = soup.find('table', class_='standard').find('tbody')
-all_countries = countries_table.findAll('tr')
-db = {}
+    page = requests.get(url).text
+    soup = BeautifulSoup(page, 'html.parser')
+    countries_table = soup.find('table', class_='standard').find('tbody')
+    all_countries = countries_table.findAll('tr')
+    db = {}
 
-for row in all_countries[1:]:
-    name = row.find('span', class_="wrap")
-    population = row.find_all_next('td')[2].text.replace(" ", "")
-    print(population)
-    if name is None:
-        name = row.find_all_next('a')[1]
-    # print(name)
-    db[name.text] = int(population.replace(" ", ""))
+    for row in all_countries[1:]:
+        name = row.find('span', class_="wrap")
+        population = row.find_all_next('td')[2].text.replace(" ", "")
+        print(population)
+        if name is None:
+            name = row.find_all_next('a')[1]
+        # print(name)
+        db[name.text] = int(population.replace(" ", ""))
 
-with open("countries_population.json", "w", encoding="utf-8") as file:
-    json.dump(db, file, indent=4, ensure_ascii=False)
+    with open("countries_population.json", "w", encoding="utf-8") as file:
+        json.dump(db, file, indent=4, ensure_ascii=False)
 
-with open("countries_population.json", "r", encoding="utf-8") as file:
-    db = json.load(file)
-    print(db)
-
-
-
-
-
+    with open("countries_population.json", "r", encoding="utf-8") as file:
+        db = json.load(file)
+        print(db)
 
     # img_src = "https:" + row.find('img')["src"]
     # print(img_src)
